c105647b62460cd8a734121ad0d12995f75f9a1f
ankul07 <96623109+ankul07@users.noreply.github.com>
2022-10-03 15:11:11 +0100

    Update database.js

--- models/database/database.js
+++ models/database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://ankur:ankur@cluster0.xtyv4.mongodb.net/ankulportfolio?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect("mongodb+srv://username:<password>@cluster0.xtyv4.mongodb.net/ankulportfolio?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("successful connected");
 }).catch((err)=>{
     console.log("not connection");
